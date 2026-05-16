2f4ed206d3ebea18d1abfb231a13d07869419542
OnurSahin111 <onur32724@outlook.com>
2026-05-16 03:57:01 +0300

    v3

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -55,6 +55,7 @@ new #[Layout('layouts.guest')] class extends Component {
             </label>
         </div>
 
+        {{-- Alt Kısım: Kayıt Ol linki ve Giriş aksiyonları --}}
         <div class="flex items-center justify-between mt-4">
             @if (Route::has('register'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}" wire:navigate>
